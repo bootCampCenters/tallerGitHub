20aa49bbda13f1c3d8786b8712659fcedb4c6f47
josefranciscoparra <36402365+josefranciscoparra@users.noreply.github.com>
2018-02-12 13:51:29 +0100

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
-	public static void main(String [] args) {
-		System.out.println(Hola bootCampers!);
+public Class HelloWorld {
+	public static void main(String []args) {
+		System.out.println("Hola bootCampers!");
 	}
 }
