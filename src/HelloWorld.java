2779644657784e7939d4f728e7c229ead00d1b3f
Julian Lopez <julianlopezjimenez@gmail.com>
2019-01-21 13:31:52 +0100

    UPDATE: Corregido error

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ import java.lang.String;
 
 public class HelloWorld {
 	public static void main(String [] args) {
-		System.out.println(Hola bootCampers!);
+		System.out.println("Hola bootCampers!");
 	}
 }
