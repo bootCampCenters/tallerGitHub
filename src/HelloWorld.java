47ddbe9cdc68a8b9384d1efa32cb4ad2c12ac0e2
jesusdavidguti <jesusdavid.guti@gmail.com>
2019-01-21 13:27:10 +0100

    test java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
-public class HelloWorld {
-	public static void main(String [] args) {
+public class HelloWorld 
+{
+	public static void main(String [] args) 
+	{
 		System.out.println(Hola bootCampers!);
 	}
 }
